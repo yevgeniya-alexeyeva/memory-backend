941c525cd7753361e04631ca17d232312477b867
Yevgeniya Alexeyeva <y.alekseyeva@gmail.com>
2021-10-28 09:29:38 +0200

    add socket connection

---
+++ app.js
@@ -0,0 +1,53 @@
+const createError = require("http-errors");
+const cors = require("cors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+app.use(logger("dev"));
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+io.use((socket, next) => {
+  if (isValid(socket.request)) {
+    next();
+  } else {
+    next(new Error("invalid"));
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = { io, http };
